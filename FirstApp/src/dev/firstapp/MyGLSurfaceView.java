da73ce69b49f8ea5168c15b2a3976a2c8c4b1cf2
gleidsoncosta <gmendes.costa@hotmail.com>
2014-09-30 21:59:58 -0700

    problem with opengl solved. The problem was in the order of on line of code on the OpenGLSurfaceView, we have to say the version first - google doesn`t explain it. So, now it is fine to work with opengl. It just make a black background now.

--- FirstApp/src/dev/firstapp/MyGLSurfaceView.java
+++ FirstApp/src/dev/firstapp/MyGLSurfaceView.java
@@ -8,8 +8,8 @@ public class MyGLSurfaceView extends GLSurfaceView{
 	public MyGLSurfaceView(Context context){
 		super(context);
 		
-		setRenderer(new MyRenderer());
 		setEGLContextClientVersion(2);
+		setRenderer(new MyRenderer());
 		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 	}
 
